a88f16404fa92969ed073434dc0489744fd1e549
João Amadeu <jmamadeu2000@gmail.com>
2021-10-27 19:01:30 +0100

    chore: add api provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { QueryClientProvider } from 'react-query';
 import { App } from './App';
+import { queryClient } from './clients/api';
 import { theme } from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
